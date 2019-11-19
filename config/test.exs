c24cfa89de6e6a85084c4f973f93ca1c5fa2d1c5
André Souza <andrestephano@gmail.com>
2019-11-18 21:01:31 -0300

    chore(initial_setup): Clean up mix.exs and config files. Moves database credentials to env.

--- config/test.exs
+++ config/test.exs
@@ -1,18 +1,14 @@
 use Mix.Config
 
-# Configure your database
 config :banking, Banking.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("PG_USER", "postgres"),
+  password: System.get_env("PG_PASSWORD", "postgres"),
   database: "banking_test",
-  hostname: "localhost",
+  hostname: System.get_env("PG_HOSTNAME", "localhost"),
   pool: Ecto.Adapters.SQL.Sandbox
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
 config :banking, BankingWeb.Endpoint,
   http: [port: 4002],
   server: false
 
-# Print only warnings and errors during test
 config :logger, level: :warn

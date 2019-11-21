0ce6679bc08fabc61fbeb28f404814360ecc58ca
André Stephano <andrestephano@gmail.com>
2019-11-21 16:03:59 -0300

    feature: adds a snippet of accounts schemas to AccountManager

--- lib/banking/account_management.ex
+++ lib/banking/account_management.ex
@@ -1,9 +1,30 @@
 defmodule Banking.AccountManagement do
   @type account_or_email_verification_code :: Account.t() | String.t()
+  @type account = Account.t()
 
-  @callback create(RegistrationSchema.t()) :: {:ok, Account.t()} | {:error, Ecto.Changeset.t()}
-  @callback get(Integer.t()) :: Account.t() | Exception.t()
-  @callback get_by_public_id(String.t()) :: {:ok, Account.t()} | {:error, :account_not_found}
+  @callback create(RegistrationSchema.t()) :: {:ok, account} | {:error, Ecto.Changeset.t()}
+  @callback get(Integer.t()) :: account | Exception.t()
+  @callback get_by_public_id(String.t()) :: {:ok, account} | {:error, :account_not_found}
   @callback validade_email(account_or_email_verification_code) :: {:ok, account} | {:error, :account_not_found}
   @callback public_update(account) :: {:ok, account} | {:error, :account_not_found}
+  @callback enable(account) :: :ok
+  @callback disable(account) :: :ok
 end
+
+
+# # User
+# name
+# birthdate
+# password_hash
+# email
+# pending_email
+# email_verification_code
+# document_type
+# document_id
+# account_id
+
+# # Account
+# public_id
+# account_type
+# status (pending | active | not)
+
---
+++ plans/conveniences.md
@@ -0,0 +1,9 @@
+# Conveniences
+
+For the propose of this project, there are some conveniences. Please check them before using in production:
+
+    * Single account per user
+    * Single document requirement per use
+    * Accounts status are enabled just after creation
+    * No document validation
+    * No distinction between business and personal account

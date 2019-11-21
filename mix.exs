7e16206707c78e97a0929ecc2343b94c3ca0370a
André Stephano <andrestephano@gmail.com>
2019-11-20 23:59:05 -0300

    chore: adds mix_test_watch

--- mix.exs
+++ mix.exs
@@ -36,6 +36,7 @@ defmodule Banking.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
       {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false}
     ]
   end

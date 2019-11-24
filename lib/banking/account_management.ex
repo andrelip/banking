fa7213be038111ab5145b98de03b33abdaf2f9ca
André Souza <andrestephano@gmail.com>
2019-11-24 00:42:04 -0300

    docs: Adds documentation for Banking.AccountManagement

--- lib/banking/account_management.ex
+++ lib/banking/account_management.ex
@@ -1,4 +1,8 @@
 defmodule Banking.AccountManagement do
+  @moduledoc """
+  Works as a public API to handle all the operations regarding user registration
+  and account administration.
+  """
   alias Banking.AccountManagement.Account
   alias Banking.AccountManagement.Registration
   alias Banking.AccountManagement.User
@@ -7,6 +11,17 @@ defmodule Banking.AccountManagement do
 
   import Ecto.Changeset, only: [change: 2]
 
+  @doc """
+  Register a user with the following fields:
+
+    * `name` - The FULL NAME of the user.
+    * `email`
+    * `password` - Must have length between 8 and 128 characters, contains a
+    number, an upper-case, a lower-case and a symbol
+    * `birthday`
+    * `document_id` with `document_type` - The document if for the given type
+     that could be of the following: `cpf`, `rg`, `passport` or `cnpj`.
+  """
   @spec create(Registration.t()) ::
           {:ok, %{account: Account.t(), user: User.t()}}
           | {:error, Ecto.Changeset.t(Registration.t())}
@@ -14,22 +29,40 @@ defmodule Banking.AccountManagement do
     registration |> Map.from_struct() |> Registration.insert()
   end
 
+  @doc """
+  Get an account by his id.
+  """
   @spec get(integer()) :: Account.t() | nil
   def get(account_id), do: Repo.get(Account, account_id)
 
+  @doc """
+  Get the account by the public available UUID
+  """
   @spec get_by_public_id(Ecto.UUID.t()) :: Account.t() | nil
   def get_by_public_id(uuid), do: Repo.get_by(Account, %{public_id: uuid})
 
+  @doc """
+  Block a given account
+  """
   @spec disable(Account.t()) :: Account.t()
   def disable(account) do
     account |> change(status: "inactive") |> Repo.update!()
   end
 
+  @doc """
+  Enable a given account
+  """
   @spec enable(Account.t()) :: Account.t()
   def enable(account) do
     account |> change(status: "active") |> Repo.update!()
   end
 
+  @doc """
+  Validate the email by moving `pending_email` to the main `email` field.
+
+  If the user has a pending confirmation account associated then this account
+  will be activated.
+  """
   def validate_email(user) do
     EmailVerification.validate_email(user)
   end

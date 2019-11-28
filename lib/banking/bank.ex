8e0cf22b2c944ec535d1341d82934c6bfaa73f4f
André Souza <andrestephano@gmail.com>
2019-11-27 23:07:03 -0300

    refactor: clean up the Ecto.Multi.run to make it a single line statement

--- lib/banking/bank.ex
+++ lib/banking/bank.ex
@@ -4,39 +4,25 @@ defmodule Banking.Bank do
   alias Banking.AccountManagement.Account
   alias Banking.Bank.Transaction
 
-  def deposit(target, amount) do
-    transaction_changeset = write_transaction(target, target, amount)
-
-    Ecto.Multi.new()
-    |> Ecto.Multi.insert(:bank_transaction, transaction_changeset)
-    |> Ecto.Multi.run(:change_account_amount, fn _, _ ->
-      change_account_balance(target, -amount)
-    end)
-    |> Repo.transaction()
-  end
-
   def transfer(source, target, amount) do
     transaction_changeset = write_transaction(source, target, amount)
+    reduce_from_source = fn _, _ -> change_account_balance(source, -amount) end
+    increment_target = fn _, _ -> change_account_balance(target, amount) end
 
     Ecto.Multi.new()
     |> Ecto.Multi.insert(:bank_transaction, transaction_changeset)
-    |> Ecto.Multi.run(:change_source_amount, fn _, _ ->
-      change_account_balance(source, -amount)
-    end)
-    |> Ecto.Multi.run(:change_target_amount, fn _, _ ->
-      change_account_balance(target, amount)
-    end)
+    |> Ecto.Multi.run(:reduce_from_source, reduce_from_source)
+    |> Ecto.Multi.run(:increment_target, increment_target)
     |> Repo.transaction()
   end
 
   def withdrawal(source, amount) do
     transaction_changeset = write_transaction(source, source, amount)
+    subtract_from_source = fn _, _ -> change_account_balance(source, -amount) end
 
     Ecto.Multi.new()
     |> Ecto.Multi.insert(:bank_transaction, transaction_changeset)
-    |> Ecto.Multi.run(:change_account_amount, fn _, _ ->
-      change_account_balance(source, -amount)
-    end)
+    |> Ecto.Multi.run(:subtract_from_source, subtract_from_source)
     |> Repo.transaction()
   end
 
